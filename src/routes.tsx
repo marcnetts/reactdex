df14e148d9e32a45ee4185d73b471691d05eb2a2
Marcos Zanetti <marcnetts@gmail.com>
2023-09-22 17:11:56 -0300

    Duplicate call

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,55 +3,22 @@ import { Header } from "./components/Header";
 import { Footer, SearchMonsArea } from "./components";
 import Container from "./components/Container";
 import Index from "./pages/Index";
-import { useEffect, useState } from "react";
-import axios from "axios";
 import { UserContextProvider } from "./assets/contexts/UserContext";
 import MonDetails from "./pages/MonDetails/MonDetails";
 
-interface MonDataBasic{
-  name: string,
-  order: number,
-}
-
 function AppRoutes() {
-  // const [searchInput, setSearchInput] = useState<string>("");
-  // const [isLoading, setLoading] = useState<boolean>();
-  const [monsGeneralData, setMonsGeneralData] = useState<MonDataBasic[]>([]);
-    
-  async function getAllMons() {
-    // setLoading(true);
-    var endpoints = [];
-    for (let index = 1; index < 50; index++) {
-      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${index}/`);
-    }
-    
-    var apiMonData: MonDataBasic[] = [];
-    Promise.all(endpoints.map((endpoint) => axios.get(endpoint)
-      .then((response) => {
-        apiMonData.push(response.data);
-        setMonsGeneralData(apiMonData);})));
-    // setLoading(false);
-  }
 
-  useEffect(() => {
-    if(!monsGeneralData.length)
-      getAllMons();
-  }, [])
-  
   return (
     <BrowserRouter>
       <Header />
       <Container>
-        {/* <FavoritosProvider> */}
         <UserContextProvider>
           <SearchMonsArea></SearchMonsArea>
           <Index />
             <Routes>
             {/* <Route path="/" element={<Index searchedMon={searchInput} monData={monsGeneralData} />}></Route> */}
             <Route path="/mon/:id" element={<MonDetails />}></Route>
-            {/* <Route path="/favoritos" element={<Favoritos />}></Route> */}
           </Routes>
-        {/* </FavoritosProvider> */}
         </UserContextProvider>
       </Container>
       <Footer />
